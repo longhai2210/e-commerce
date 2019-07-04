fff29c0e9c4ddca91e65c878072fe1ef98cf3749
qe <qe@qes-Mac-mini.local>
2019-07-04 11:47:37 +0700

    push 3

--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import PayPalButton from "./PayPalButton";
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
   const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
 
   return (
@@ -19,23 +20,22 @@ export default function CartTotals({ value }) {
               </button>
             </Link>
             <h5>
-            <span className="text-title">
-                subtotal : 
-            </span>
-            <strong>$ {cartSubTotal}</strong>
+              <span className="text-title">subtotal :</span>
+              <strong>$ {cartSubTotal}</strong>
             </h5>
             <h5>
-            <span className="text-title">
-                tax : 
-            </span>
-            <strong>$ {cartTax}</strong>
+              <span className="text-title">tax :</span>
+              <strong>$ {cartTax}</strong>
             </h5>
             <h5>
-            <span className="text-title">
-                total : 
-            </span>
-            <strong>$ {cartTotal}</strong>
+              <span className="text-title">total :</span>
+              <strong>$ {cartTotal}</strong>
             </h5>
+            <PayPalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
           </div>
         </div>
       </div>
